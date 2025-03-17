8c61e3de654fc1fc0f38e24dc11300911d8c42f0
aikhe <ikeandrie.ro@gmail.com>
2025-03-17 19:58:10 +0800

    feat(cord): updated cord.nvim & fixed invisible assets

--- lua/kickstart/plugins/cord.lua
+++ lua/kickstart/plugins/cord.lua
@@ -1,17 +1,24 @@
 return {
   {
     'vyfor/cord.nvim',
-    build = './build || .\\build',
-    -- event = 'VeryLazy',
+    build = function()
+      if vim.fn.has 'win32' == 1 then
+        os.execute 'bash -c "./build"'
+      else
+        os.execute './build'
+      end
+    end,
     opts = {
+      enabled = true,
+      log_level = vim.log.levels.OFF,
       editor = {
-        image = nil,
         client = 'neovim',
         tooltip = 'Neovim',
+        icon = nil,
       },
       display = {
-        show_repository = false,
-        theme = 'pastel',
+        theme = 'catppuccin',
+        flavor = 'accent',
         swap_fields = false,
         swap_icons = true,
       },
@@ -28,13 +35,11 @@ return {
         unidle_on_focus = true,
         smart_idle = true,
         details = '',
-        text = '',
         state = nil,
         tooltip = '💤',
         icon = nil,
       },
       text = {
-        -- viewing = '‎‎',
         viewing = '', -- Text to display when viewing a readonly file
         editing = '', -- Text to display when editing a file
         file_browser = '', -- Text to display when browsing files (Empty string to disable)
@@ -44,30 +49,28 @@ return {
         workspace = '', -- Text to display when in a workspace (Empty string to disable)
       },
       buttons = nil,
-      assets = {
-        lazy = { -- Vim filetype or file name or file extension = table or string
-          name = 'Lazy', -- Optional override for the icon name, redundant for language types
-          icon = 'https://example.com/lazy.png', -- Rich Presence asset name or URL
-          tooltip = 'lazy.nvim', -- Text to display when hovering over the icon
-          type = 'plugin_manager', -- One of 'language', 'file_browser', 'plugin_manager', 'lsp_manager', 'vcs' or respective ordinals; defaults to 'language'
-        },
-        ['Cargo.toml'] = 'crates',
-      },
+      -- buttons = {
+      --   {
+      --     label = 'View Repository',
+      --     url = function(opts) return opts.repo_url end,
+      --   },
+      -- },
+      assets = nil,
       variables = nil,
       hooks = {
-        on_ready = nil,
-        on_update = nil,
-        on_activity = nil,
-        on_idle = nil,
-        on_workspace_change = nil,
-        on_disconnect = nil,
+        ready = nil,
+        shutdown = nil,
+        pre_activity = nil,
+        post_activity = nil,
+        idle_enter = nil,
+        idle_leave = nil,
+        workspace_change = nil,
       },
+      plugins = nil,
       advanced = {
         plugin = {
           autocmds = true,
-          log_level = vim.log.levels.INFO,
           cursor_update = 'on_hold',
-          variables_in_functions = false,
           match_in_mappings = true,
         },
         server = {
@@ -80,9 +83,53 @@ return {
           reconnect = {
             enabled = false,
             interval = 5000,
+            initial = true,
           },
         },
       },
-    }, -- calls require('cord').setup()
+    },
+    -- event = 'VeryLazy',
+    -- opts = {
+    --   editor = {
+    --     image = nil,
+    --     client = 'neovim',
+    --     tooltip = 'Neovim',
+    --   },
+    --   display = {
+    --     show_repository = false,
+    --     theme = 'atom',
+    --     flavor = 'dark',
+    --     swap_fields = false,
+    --     swap_icons = true,
+    --   },
+    --   timestamp = {
+    --     enabled = true,
+    --     reset_on_idle = false,
+    --     reset_on_change = false,
+    --   },
+    --   idle = {
+    --     enabled = true,
+    --     timeout = 300000,
+    --     show_status = true,
+    --     ignore_focus = true,
+    --     unidle_on_focus = true,
+    --     smart_idle = true,
+    --     details = '',
+    --     state = nil,
+    --     tooltip = '💤',
+    --     icon = nil,
+    --   },
+    --   text = {
+    --     -- viewing = '‎‎',
+    --     viewing = '', -- Text to display when viewing a readonly file
+    --     editing = '', -- Text to display when editing a file
+    --     file_browser = '', -- Text to display when browsing files (Empty string to disable)
+    --     plugin_manager = '', -- Text to display when managing plugins (Empty string to disable)
+    --     lsp_manager = '', -- Text to display when managing LSP servers (Empty string to disable)
+    --     vcs = '', -- Text to display when using Git or Git-related plugin (Empty string to disable)
+    --     workspace = '', -- Text to display when in a workspace (Empty string to disable)
+    --   },
+    --   buttons = nil,
+    -- }, -- calls require('cord').setup()
   },
 }
