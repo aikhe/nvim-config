1ebf130ba75ab1494fe3d8b728e710c64f48c13d
aikhe <ikeandrie.ro@gmail.com>
2026-05-31 16:57:12 +0800

    my config needs some work man

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,46 @@
-return { -- Highlight, edit, and navigate code
+return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter', -- Changed from 'nvim-treesitter.configs'
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
+  -- Use the new top-level module
+  main = 'nvim-treesitter',
+
+  -- The old `opts` table with `highlight` and `indent` will no longer do anything.
+  -- Instead, we install parsers and use an autocmd to enable features natively.
+  init = function()
+    -- 1. Automatically turn on Treesitter features when opening files
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        -- Safely start native treesitter highlighting
+        pcall(vim.treesitter.start)
+
+        -- Enable native treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
+
+  config = function(_, opts)
+    -- 2. Setup the plugin (creates commands, handles paths)
+    require('nvim-treesitter').setup(opts)
+
+    -- 3. Explicitly install the languages you want
+    local ensure_installed = {
+      'bash',
+      'c',
+      'diff',
+      'html',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'query',
+      'vim',
+      'vimdoc',
+      'typescript',
+      'tsx',
+      'javascript',
+    }
+
+    require('nvim-treesitter').install(ensure_installed)
+  end,
 }
